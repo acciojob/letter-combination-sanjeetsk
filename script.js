8efa9420ac439d6e092d1209ca792fb5370bcd23
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-06-04 09:34:55 +0530

    update

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
 function letterCombinations(input_digit) {
   //Complete the function
+  if(input_digit.length === 0){
+    return [];
+  }
+
+  const digitMap = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
+
+  const combinations = [];
+
+  function generateCombinations(current, index){
+
+    if(index === input_digit.length){
+      console.log(current + "***");
+      combinations.push(current);
+      return;
+    }
+
+    console.log(current + "----");
+
+    const letters = digitMap[input_digit[index]];
+    console.log(letters + "----");
+
+    for(let i=0; i < letters.length; i++){
+      console.log(current + " " + letters[i]);
+      generateCombinations(current + letters[i], index+1);
+    }
+    
+  } 
+
+  generateCombinations('', 0);
+
+  return combinations.sort();
+  
 }
 
 module.exports = letterCombinations;
